c52b7d14005d963006862126f375294272102fc1
BenMack8413 <ben.mackaway@proton.me>
2025-09-04 00:00:28 +1000

    i want to be done with this

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,10 +2,9 @@ import js from "@eslint/js";
 import globals from "globals";
 import css from "@eslint/css";
 import { defineConfig } from "eslint/config";
-import prettierPlugin from "eslint-plugin-prettier";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js, prettier: prettierPlugin }, extends: ["js/recommended", "plugin:prettier/recommended"], languageOptions: { globals: {...globals.browser, ...globals.node} }, rules: {"prettier/prettier": "error"} },
+  { files: ["**/*.{js,mjs,cjs}"], extends: ["js/recommended", "plugin:prettier/recommended"], languageOptions: { globals: {...globals.browser, ...globals.node} }, rules: {"prettier/prettier": "error"} },
   { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
   { files: ["**/*.css"], plugins: { css }, language: "css/css", extends: ["css/recommended"] },
 ]);
