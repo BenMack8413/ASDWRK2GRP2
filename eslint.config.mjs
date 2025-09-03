20bdb1ffbb33b954fca3cdcb9b61b79cc9e160da
BenMack8413 <ben.mackaway@proton.me>
2025-09-03 23:55:57 +1000

    trying again

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,7 @@ import js from "@eslint/js";
 import globals from "globals";
 import css from "@eslint/css";
 import { defineConfig } from "eslint/config";
+import prettierPlugin from "eslint-plugin-prettier";
 
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs}"], plugins: { js, prettier: prettierPlugin }, extends: ["js/recommended", "plugin:prettier/recommended"], languageOptions: { globals: {...globals.browser, ...globals.node} }, rules: {"prettier/prettier": "error"} },
