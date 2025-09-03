85b966e9814ce55d1acb3bf0161c5f3f0cc89993
BenMack8413 <ben.mackaway@proton.me>
2025-09-04 00:02:42 +1000

    testing yaml maybe done?

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,24 @@
-import js from "@eslint/js";
 import globals from "globals";
-import css from "@eslint/css";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], extends: ["js/recommended", "plugin:prettier/recommended"], languageOptions: { globals: {...globals.browser, ...globals.node} }, rules: {"prettier/prettier": "error"} },
-  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
-  { files: ["**/*.css"], plugins: { css }, language: "css/css", extends: ["css/recommended"] },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    extends: ["@eslint/js/recommended", "plugin:prettier/recommended"],
+    languageOptions: {
+      globals: { ...globals.browser, ...globals.node },
+    },
+    rules: {
+      "prettier/prettier": "error",
+    },
+  },
+  {
+    files: ["**/*.js"],
+    languageOptions: { sourceType: "commonjs" },
+  },
+  {
+    files: ["**/*.css"],
+    extends: ["@eslint/css/recommended"],
+    language: "css/css",
+  },
 ]);
