fce122561631e1858cb39675bf2e1637358cb825
JHTech <160552777+JacksonHtech@users.noreply.github.com>
2025-10-02 14:08:22 +1000

    Revert "Merge branch 'dev' of https://github.com/BenMack8413/ASDWRK2GRP2 into dev"
    This reverts commit e943119b7a0630dec52e6789ea1cc054a5c3d33e, reversing
    changes made to 205b31c7cfa20e7ad7160a64a64546f15682852f.

--- backend/api/index.js
+++ backend/api/index.js
@@ -4,8 +4,6 @@ const createUserRouter = require('./user');
 const createIncomeRouter = require('./income');
 const createSimpleIncomeRouter = require('./incomes_simple');
 const createSettingRouter = require('./settings');
-const createSavingGoalsRouter = require('./savingGoals');
-const createUpcomingPaymentsRouter = require('./upcomingPayments');
 
 // const sampleApiRouter = require('./sampleapi');
 
@@ -18,8 +16,6 @@ module.exports = function createApiRouter(db /*, createTransactionAtomic*/) {
     router.use('/income', createIncomeRouter(db));
     router.use('/incomes', createSimpleIncomeRouter());
     router.use('/settings', createSettingRouter(db));
-    router.use('/saving-goals', createSavingGoalsRouter(db));
-    router.use('/upcoming-payments', createUpcomingPaymentsRouter(db));
 
     // router.use('/sampleApiRouter', sampleApiRouter(db));
 
